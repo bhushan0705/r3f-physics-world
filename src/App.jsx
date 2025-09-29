e3f4adc2701f1a9abbefbbc9efe0230e7c5d836a
bhushan <bhushan752002gmail.com>
2025-09-29 20:25:17 +0530

    checks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import { Suspense } from 'react';
@@ -5,15 +6,11 @@ import Project2 from './Project2';
 import './App.css';
 
 function App() {
+
   return (
-    <Canvas shadows camera={{ position: [0, 30, 15], fov: 60 }}>
-      {/* Set background color properly */}
-      <color attach="background" args={['#7cccecff']} />
-
-      {/* Add fog */}
+    <Canvas shadows camera={{ position: [0, 30, 15], fov:60 }} background={"#7cccecff"}>
       <fog attach="fog" args={['#a0c4ff', 10, 100]} />
-
-      <Suspense fallback={null}>
+      <Suspense>
         <Physics>
           <Project2 />
         </Physics>
